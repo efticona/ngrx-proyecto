0b5dcefe0241842c364756dab5c7c2f6a803f2e6
eric <favioticona@gmail.com>
2019-06-04 22:20:53 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Store, Action } from "@ngrx/store";
+import {
+  IncrementarAction,
+  DecrementarAction
+} from "./contador/contador.actions";
+interface AppState {
+  contador: number;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'redux-app';
+  contador: number;
+
+  constructor(private store: Store<AppState>) {
+    // this.contador = 10;
+    this.store.select("contador").subscribe(contador => {
+      this.contador = contador;
+      console.log(contador);
+    });
+  }
+
+  incrementar() {
+    // this.contador++;
+    const accion = new IncrementarAction();
+
+    this.store.dispatch(accion);
+  }
+  decrementar() {
+    // this.contador--;
+    const accion = new DecrementarAction();
+    this.store.dispatch(accion);
+  }
 }
